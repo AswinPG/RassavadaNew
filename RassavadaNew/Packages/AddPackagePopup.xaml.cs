9f9165c938d12d1c8cb7c2dfd150cd4466f5b231
Aswin P G <aswinpg@outlook.in>
2019-12-08 20:54:43 +0530

    A lot of changes

---
+++ RassavadaNew/Packages/AddPackagePopup.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RassavadaNew.Packages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddPackagePopup : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public AddPackagePopup()
+        {
+            InitializeComponent();
+        }
+
+        private async void Next(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EditPackagePage());
+        }
+    }
+}
